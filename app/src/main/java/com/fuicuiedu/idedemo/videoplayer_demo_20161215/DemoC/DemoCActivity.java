aa82a28cb74c9f07eabf337ce2c699a76787ce55
wxcican <wxcican@qq.com>
2016-12-16 11:59:13 +0800

    VideoView完成

--- app/src/main/java/com/fuicuiedu/idedemo/videoplayer_demo_20161215/DemoC/DemoCActivity.java
+++ app/src/main/java/com/fuicuiedu/idedemo/videoplayer_demo_20161215/DemoC/DemoCActivity.java
@@ -2,14 +2,26 @@ package com.fuicuiedu.idedemo.videoplayer_demo_20161215.DemoC;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 import com.fuicuiedu.idedemo.videoplayer_demo_20161215.R;
+import com.fuicuiedu.idedemo.videoplayer_demo_20161215.VideoUrlRes;
 
 public class DemoCActivity extends AppCompatActivity {
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_c);
+
+        videoView = (VideoView) findViewById(R.id.main_c_vv);
+
+        //设置数据源
+        videoView.setVideoPath(VideoUrlRes.getTestUrl1());
+
+        //开始播放
+        videoView.start();
+
     }
 }
--- app/src/main/java/com/fuicuiedu/idedemo/videoplayer_demo_20161215/DemoD/DemoDActivity.java
+++ app/src/main/java/com/fuicuiedu/idedemo/videoplayer_demo_20161215/DemoD/DemoDActivity.java
@@ -4,12 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.fuicuiedu.idedemo.videoplayer_demo_20161215.R;
+import com.fuicuiedu.idedemo.videoplayer_demo_20161215.VideoUrlRes;
+
+import io.vov.vitamio.widget.VideoView;
 
 public class DemoDActivity extends AppCompatActivity {
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_d);
+
+        videoView = (VideoView) findViewById(R.id.main_d_vv);
+
+        videoView.setVideoPath(VideoUrlRes.getTestUrl1());
+
+        videoView.start();
+
     }
 }
